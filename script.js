4b7c503e1b5d73ae228f454cff3aa3c1c64351db
SALSABEEL99 <sabeelbaloch21@gmail.com>
2024-03-22 14:33:43 +0500

    chapter  2 & 4 complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Chapter 2 
+//Chapter 2
 
 //Question # 1:
 let username;
@@ -25,6 +25,8 @@ Mybiodata += "Qualification: " + Qualification + "\n";
 Mybiodata += "Hobbies: " + Hobbies + "\n";
 alert(Mybiodata);
 //
+// Question # 5:
+alert("pizza\npizz\npiz\npi\np");
 
 //Question # 6:
 let email = "sabeelbaloch21@gmail.com@gmail.com";
@@ -40,7 +42,7 @@ alert("I am currently reading the book: '" + book + "'");
 //
 
 //Question # 8:
-document.write("Yah! I can write HTML content through JavaScript")
+document.write("Yah! I can write HTML content through JavaScript");
 //
 
 //Question # 9:
@@ -50,4 +52,37 @@ alert(specialstring);
 
 // Display the string in the browser
 document.write(specialstring);
+//
+
+// Chapter # 4:
+// Question 1;
+let variable1, variable2, variable3;
+
+//Question # 2
+//legal variable
+/*let salsabeel;
+let sabeel99;
+let $sabeel;
+let _sabeel;
+let sa99beel;
+//
+
+//illegal variable
+let sab eel
+let 99sabeel
+let "sabeel"
+let @sabeel
+let (sabeel)*/
+//
+
+//Question # 3
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write(
+  "<p></p>Variable names can only contain ,numbers,$ and _ . For example : $my_1stVariable</p>"
+);
+document.write(
+  "<p>Variables must begin with a letter,$ or_ . For example : $name, _name or name</p>"
+);
+document.write("<p></p>Variable names are case-sensitive</p>");
+document.write("<p></p>Variable names should not be JavaScript keywords</p>");
 //
